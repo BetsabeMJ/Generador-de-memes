80d718d14a6df912d733b527db380684913ee901
Betsabe Martinez <betsabe.martinez.jasso@gmail.com>
2024-03-14 18:57:20 -0600

    ultimo cierre

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hola!")
+// console.log("Hola!");
+const darkMode = document.querySelector(".dark__mode");
+console.log(darkMode);
+
+// darkMode.addEventListener("click", () => {
+//   document.classList.add(".dark__mode");
+// });
