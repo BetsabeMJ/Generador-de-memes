9a2a9fc755b0dc342b1302a26c0649aa26cb2b93
Betsabe Martinez <betsabe.martinez.jasso@gmail.com>
2024-07-13 00:42:32 -0600

    creación de modo oscuro

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-console.log("Hola!");
-const darkMode = document.querySelector(".dark__mode");
-console.log(darkMode);
+//console.log("Hola!");
+//console.log(darkMode);
 
 // darkMode.addEventListener("click", () => {
 //   document.classList.add(".dark__mode");
 // });
+
+//MEDIO FUNCIONA EL MODO OSCURO, PERO FALTA QUE LOS ASIDES SE PINTEN DE NEGRO Y EL FONDO DE LOS INPUST
+const bodyColor = document.querySelector(".body");
+const darkMode = document.querySelector('.dark-mode')
+
+
+const cambiarColor = () => {
+  if (bodyColor.classList.contains('body')) {
+    bodyColor.classList.replace('body', 'dark-mode')
+    changeColor.innerText = 'Modo claro'
+  } else {
+    bodyColor.classList.replace('dark-mode', 'body')
+    changeColor.innerText = 'Modo oscuro'
+  }
+}
+
+let changeColor = document.getElementById('dark--mode--button')
+let darkIcon = document.querySelector('.fa-solid fa-lightbulb')
+
+changeColor.addEventListener('click', cambiarColor);
